24f21071a733c90e5bb6fd6104ea3ccbbb05c21f
francescoP02 <file.partipilo@gmail.com>
2022-05-31 15:55:50 +0200

    Completed review section with dynamic radio button

--- src/components/AppReviews.vue
+++ src/components/AppReviews.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
+  <div class="ms-container">
       <div class="ms-review" v-for="(item,index) in review" :key="index" :class="{active : index === currentItem}">
 
       <img :src="require(`../assets/img/${item.img}`)" alt="">
       <span>{{item.review}}</span>
       <span>{{item.name}}, Theme Fusion</span>
       </div>
+
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="0" checked="checked" @click="clickedItem(val())">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="0" checked="checked" @click="clickedItem(0)">
         <label class="form-check-label" for="inlineRadio1">1</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="1" @click="clickedItem(val())">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="1" @click="clickedItem(1)">
         <label class="form-check-label" for="inlineRadio2">2</label>
       </div>
 
@@ -25,7 +26,6 @@ export default {
   data: function() {
       return {
           currentItem: 0,
-          value: 0,
           review: [
               {
                   img: "man_testimonial.png",
@@ -41,9 +41,10 @@ export default {
       }
   },
   methods: {
-    clickedItem : function() {
-        console.log(value);
+    clickedItem : function(number) {
+        this.currentItem = number;
   },
+  }
 }
 </script>
 
@@ -51,12 +52,30 @@ export default {
 @import "../style/common.scss";
 @import "../style/variables.scss";
 
-.ms-review {
-    display: none;
+.ms-container {
+  background-image: url(../assets/img/testimonials_home_1_bg.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: #ffffff;
+  text-align: center;
+  padding: 2rem 0;
+  margin-top: 4rem;
 
-    .active {
-        display: block;
+  .ms-review {
+    display: none;
+    width: 80%;
+    margin: 0 auto;
+    padding-bottom: 2rem;
+
+    span {
+      display: block;
     }
+  }
+
+  .active {
+      display: block;
+  }
 }
 
 </style>
