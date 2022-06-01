439e552018328828491a1ca027c656fd1a3fad3a
francescoP02 <file.partipilo@gmail.com>
2022-06-01 11:37:50 +0200

    Added design to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="ms-footer">
       <div class="container">
-        <ul class="row row-cols-4 d-flex">
-          <li>
-            <ul>
-              <li><img src="../assets/img/classic_shop_logo1x.png" alt=""></li>
+        <ul class="row row-cols-4 d-flex ms-col">
+          <li class="ms-contacts">
+            <ul class="d-flex">
+              <li class="ms-footer-logo"><img src="../assets/img/classic_shop_logo_footer.png" alt=""></li>
               <li>12345 North Main Street,</li>
               <li>New York, NY 555555</li>
               <li>Phone: 1.800.555.6789</li>
@@ -12,14 +12,14 @@
               <li>Web: Theme-fusion.com</li>
               <li>
                 <ul class="ms-social d-flex">
-                  <li class="align-items-center" v-for="(item, index) in iconSocial" :key="index"><i class="fab" :class="item.icon"></i></li>
+                  <li class="align-items-center px-1" v-for="(item, index) in iconSocial" :key="index"><i class="fab" :class="item.icon"></i></li>
                 </ul>
               </li>
             </ul>
           </li>
-          <li>
+          <li class="ms-top-rated">
             <ul>
-              <li>TOP RATED PRODUCTS</li>
+              <li class="ms-title">TOP RATED PRODUCTS</li>
               <li class="ms-card" v-for="(item,index) in products" :key="index">
                 <ul v-if="item.top === true" class="d-flex ms-row">
                   <li class="ms-text">
@@ -35,16 +35,20 @@
               </li>
             </ul>
           </li>
-          <li>
+          <li class="ms-posts">
             <ul>
-              <li>RECENT POSTS</li>
-              <li v-for="(item,index) in posts" :key="index">{{item.post}}</li>
+              <li class="ms-title">RECENT POSTS</li>
+              <li class="ms-post" v-for="(item,index) in posts" :key="index"><span><i class="fas fa-angle-right"></i></span>{{item.post}}</li>
             </ul>
           </li>
-          <li>
+          <li class="ms-tags">
             <ul>
-              <li>TAGS</li>
-              <li v-for="(item,index) in tags" :key="index">{{item.name}}</li>
+              <li class="ms-title d-block">TAGS</li>
+              <li>
+                <ul class="d-flex flex-wrap ms-tag-group">
+                  <li class="ms-tag" v-for="(item,index) in tags" :key="index">{{item.name}}</li>
+                </ul>
+              </li>
             </ul>
           </li>
         </ul>
@@ -166,6 +170,119 @@ ul {
 
 li {
   display: inline-block;
+}
+
+.ms-footer {
+  background-color: #2b2e32;
+  color: #ffffff;
+
+  .ms-col {
+
+    padding-top: 3rem;
+
+    ul {
+      padding-left: 0;
+      .ms-title {
+        padding-bottom: 2rem;
+      }
+    }
+
+    .ms-contacts {
+      ul {
+        flex-direction: column;
+
+        .ms-footer-logo {
+          padding-bottom: 3rem;
+
+          img {
+            width: 50%;
+          }
+
+        }
+
+        li {
+          padding-top: .5rem;
+          color: $footer-color;
+
+          .ms-social {
+            flex-direction: row;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+
+    .ms-top-rated {
+      color: $footer-color;
+
+      .ms-card {
+          display: block;
+
+          .ms-row {
+              border-bottom: 1px solid grey;
+              padding-left: 0;
+              padding: 1rem 0;
+              cursor: pointer;
+
+              .ms-text {
+                  width: 70%;
+                  flex-direction: row;
+                  padding-left: 0;
+
+                  .ms-name {
+                      margin-right: .3rem;
+                  }
+                  .ms-review {
+                      display: block;
+                      color: #427ed1;
+                      padding: .5rem 0;
+                  }
+                  .ms-original {
+                      text-decoration: line-through;
+                      font-weight: 700;
+                  }
+                  .ms-price {
+                      font-weight: 500;
+                  }
+              }
+
+              .ms-img {
+                  width: 30%;
+                  img {
+                      width: 100%;
+                      padding: .5rem;
+                  }
+              }
+          }
+      }
+    }
+
+    .ms-posts {
+      color: $footer-color;
+
+      .ms-post {
+        border-bottom: 1px solid $footer-color;
+        padding: .8rem 0;
+        cursor: pointer;
+      }
+      span {
+        padding-right: .4rem;
+      }
+    }
+
+    .ms-tags {
+      color: $footer-color;
+      .ms-tag {
+        padding: .4rem;
+        border: 1px solid $footer-color;
+        cursor: pointer;
+      }
+
+      .ms-tag-group {
+        gap: 10px;
+      }
+    }
+  }
 }
 
 </style>
